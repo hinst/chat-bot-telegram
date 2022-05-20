80c65176c40932259d3e057667d557e927713b6a
hinst <hinstance@yandex.ru>
2022-05-20 23:44:39 +0200

    Adding bot stuff

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,15 @@
 import 'source-map-support/register';
+import fs from 'fs';
+import { Context, Telegraf } from 'telegraf';
+import { Update } from 'typegram';
 
-class App {
-    run() {
-    }
-}
+const API_TOKEN = fs.readFileSync('./token.txt').toString();
+const bot: Telegraf<Context<Update>> = new Telegraf(API_TOKEN);
+
+bot.start(context => {
+    context.reply('Hello. My name is Alex Sharp.');
+});
+
+bot.on('text', context => {
+    const incomingMessage = context.message.text;
+});
