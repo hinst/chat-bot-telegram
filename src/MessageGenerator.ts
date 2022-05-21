0c1645c406677b8de54c47ef677e424628a4c161
hinst <hinstance@yandex.ru>
2022-05-21 20:42:02 +0200

    Updating message generator

--- src/MessageGenerator.ts
+++ src/MessageGenerator.ts
@@ -1,6 +1,9 @@
 import lodash from 'lodash';
 import log4js from 'log4js';
 
+const OPTIMAL_CHAIN_MIN_LENGTH = 5;
+const OPTIMAL_CHAIN_MAX_LENGTH = 10;
+
 export default class MessageGenerator {
 
     logger = log4js.getLogger('MessageGenerator');
@@ -13,14 +16,60 @@ export default class MessageGenerator {
     generateResponse(message: string): string {
         const words = getWords(message);
         const desiredWord = words.length ? words[0] : '';
+        const messages1 = this.findMessageCandidates(desiredWord);
+        const message1 = lodash.sample(messages1);
+        if (message1) {
+            let desiredWordIndex = message1.indexOf(desiredWord);
+            if (desiredWordIndex >= 0) {
+                const nextTopWordIndex = this.findNextTopWordIndex(message1, desiredWordIndex);
+            }
+        }
+        return 'Unable to generate a response.';
+    }
 
+    private findChain(message: string[], index: number, direction: number): string[] {
+        const sentence = [message[index]];
+        while (sentence.length < OPTIMAL_CHAIN_MAX_LENGTH) {
+            index += direction;
+            if (index < 0 || index >= message.length)
+                break;
+            const word = message[index];
+            if (this.topWords.includes(word) && sentence.length >= OPTIMAL_CHAIN_MIN_LENGTH)
+                break;
+            if (word == '.' && sentence.length >= OPTIMAL_CHAIN_MIN_LENGTH)
+                break;
+            if (word != '.')
+                if (direction > 0)
+                    sentence.push(word);
+                else
+                    sentence.unshift(word);
+        }
+        return sentence;
+    }
+
+    private findNextTopWordIndex(message: string[], index: number) {
+        for (let i = index + 1; i < message.length; i++)
+            if (this.topWords.includes(message[i]))
+                return i;
+        return -1;
+    }
+
+    private findPreviousTopWordIndex(message: string[], index: number) {
+        for (let i = index - 1; 0 <= i; i--)
+            if (this.topWords.includes(message[i]))
+                return i;
+        return -1;
+    }
+
+    private findMessageCandidates(desiredWord: string) {
         let messages = this.messages.filter(message => message.some(word => word == desiredWord));
         if (messages.length == 0)
             messages = this.messages.filter(message => message.some(word => word.includes(desiredWord)));
         if (messages.length == 0)
             messages = this.messages.filter(message => this.topWords.some(word => message.includes(word)));
-
-
+        if (messages.length == 0)
+            messages = this.messages;
+        return messages;
     }
 }
 
@@ -44,4 +93,12 @@ function getWords(text: string) {
 
 function isLetter(text: string) {
     return text.toString().toLowerCase() != text.toString().toUpperCase();
+}
+
+function getRandomInteger(limit: number) {
+    return Math.floor(Math.random() * limit);
+}
+
+function formatSentence(message: string[]): string {
+    return lodash.upperFirst(message.join(' ')) + '.';
 }
