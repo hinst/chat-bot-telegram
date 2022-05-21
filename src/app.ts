3306e8ab0da587edeb97525015761f8e145d5fa8
hinst <hinstance@yandex.ru>
2022-05-21 11:32:14 +0200

    Check allowed user

--- src/app.ts
+++ src/app.ts
@@ -3,13 +3,24 @@ import fs from 'fs';
 import { Context, Telegraf } from 'telegraf';
 import { Update } from 'typegram';
 
-const API_TOKEN = fs.readFileSync('./token.txt').toString();
-const bot: Telegraf<Context<Update>> = new Telegraf(API_TOKEN);
+const apiToken = fs.readFileSync('./token.txt').toString();
+const bot: Telegraf<Context<Update>> = new Telegraf(apiToken);
+const allowedUserIds: number[] = JSON.parse(fs.readFileSync('./allowed-user-ids.json').toString());
+if (!allowedUserIds.length)
+    console.warn("There are no allowed user ids");
 
 bot.start(context => {
     context.reply('Hello. My name is Alex Sharp.');
 });
 
 bot.on('text', context => {
-    const incomingMessage = context.message.text;
+    const senderId = context.message?.from?.id;
+    if (allowedUserIds.includes(senderId)) {
+        const incomingMessage = context.message.text;
+        context.reply("Your message was: " + incomingMessage);
+    } else {
+        context.reply("Error: you are not authorized to use this bot. Your id is " + senderId);
+    }
 });
+
+bot.launch();
